e6056759a033d53a5357ae42c6989b33ac1c4c74
Vadim Shurman <vadym.shurman@ekreative.com>
2016-01-27 16:02:35 +0200

    init + second  step

---
+++ tests/karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-route/angular-route.js',
+      'node_modules/angular-resource/angular-resource.js',
+      'node_modules/angular-animate/angular-animate.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'app/**/*.js',
+      'tests/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome', 'Firefox'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
