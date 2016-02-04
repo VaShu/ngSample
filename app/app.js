2a2437965fde45a8d2a98612814b4f9df8ccf52a
Vadim Shurman <vadym.shurman@ekreative.com>
2016-02-04 17:04:01 +0200

    refactoring

--- app/app.js
+++ app/app.js
@@ -1,8 +1,15 @@
 'use strict';
 angular.module('myApp', [
+  'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
   'components',
   'treebuilder'
 ])
+    .config(['$routeProvider', function($routeProvider) {
+      // Routes will be here
+    }])
+    .controller( 'DateCtrl', ['$scope', function($scope) {
+      $scope.currentdate = new Date();
+    }]);
